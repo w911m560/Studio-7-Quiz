c95249a9d5db8fc656308fe8109ef4b6f072c2f3
William Morrison <williamchrismorrison@gmail.com>
2020-06-10 12:26:54 -0700

    Add project files.

---
+++ Quiz/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizNS
+{
+    public class Question
+    {
+        private static int nextQuestionNumber = 1;
+        public readonly int questionNumber
+        public string ClassName { get; set; }
+        public string ClassName { get; set; }
+        public string ClassName { get; set; }
+    }
+}
